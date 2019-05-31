97928d5463ce5a747f2702ce7acf71ef831fe6f0
Keabetswe Justice MODISE <kmodise@c4r6s1.wethinkcode.co.za>
2019-05-31 13:08:43 +0200

    modccpy

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -6,7 +6,7 @@
 /*   By: kmodise <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/26 15:01:53 by kmodise           #+#    #+#             */
-/*   Updated: 2019/05/31 10:23:35 by kmodise          ###   ########.fr       */
+/*   Updated: 2019/05/31 13:08:03 by kmodise          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,23 @@
 
 void	*ft_memccpy(void *paste, const void *copy, int c, size_t n)
 {
-	char	*ptrpaste;
-	char	*ptrcopy;
+	unsigned char	*ptrpaste;
+	unsigned char	*ptrcopy;
 	size_t	u;
+	unsigned char C;
 
 	u = 0;
 	ptrpaste = (unsigned char *)paste;
 	ptrcopy = (unsigned char *)copy;
-	while (n > 0 && ptrcopy[u] != (char)c)
+	C = (unsigned char)c;
+	while (u < n)
 	{
-		if (ptrcopy[u] == '\0')
-		{
-			break ;
-		}
 		ptrpaste[u] = ptrcopy[u];
+		if (ptrcopy[u] == C)
+		{
+			return (ptrpaste[u + 1]);
+		}
 		u++;
-		n--;
 	}
-	ptrpaste[u] = '\0';
-	return (paste);
+	return (NULL);
 }
