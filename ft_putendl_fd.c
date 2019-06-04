c941b292388296ed145642b6fa55818a1e3613c0
Keabetswe Justice MODISE <kmodise@c4r6s4.wethinkcode.co.za>
2019-06-04 16:36:37 +0200

    push

--- ft_putendl_fd.c
+++ ft_putendl_fd.c
@@ -1,34 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   test.c                                             :+:      :+:    :+:   */
+/*   ft_putendl_fd.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: kmodise <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/05/21 11:32:29 by kmodise           #+#    #+#             */
-/*   Updated: 2019/06/04 16:31:48 by kmodise          ###   ########.fr       */
+/*   Created: 2019/06/04 16:14:25 by kmodise           #+#    #+#             */
+/*   Updated: 2019/06/04 16:31:17 by kmodise          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
-#include <stdlib.h>
 
-int		main(void)
+void	ft_putendl_fd(char const *s, int fd)
 {
-//	char s[] = "the";
-	//char s2[] = "	theshininglight";
-	char s3[] = " the\n shining ";
-	//char		p;
-	//char		t;
-
-//	char t[] = "ooo00000000";
-
-	//p = putchar_fd('t', 2);
-	//printf("%d\n", i);
-	ft_putendl_fd(s3, 1);
-	//ft_strncpy(t, s2, 0);
-	//printf("%s\n", ft_strtrim(s3));
-	//printf("%s\n", ft_strtrim(s2)); 
+	if (fd >= 0){
+		write(fd, s, ft_strlen(s));
+		write(fd, "\n", 1);
+	}
 }
-
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: kmodise <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/21 10:11:25 by kmodise           #+#    #+#             */
-/*   Updated: 2019/06/04 15:31:49 by kmodise          ###   ########.fr       */
+/*   Updated: 2019/06/04 16:24:39 by kmodise          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,4 +66,5 @@ char	*ft_strjoin(char const *s1, char const *s2);
 void	ft_putendl(char const *s);
 char	*ft_strtrim(char const *s);
 int		ft_nofspc(char const *s);
+void	ft_putendl_fd(char const *s, int fd);
 #endif
