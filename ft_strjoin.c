179d0be5e9ecb5f7244872428ea4d92230aa7370
Keabetswe Justice MODISE <kmodise@c4r6s2.wethinkcode.co.za>
2019-06-14 14:44:37 +0200

    commit

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: kmodise <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/02 13:14:46 by kmodise           #+#    #+#             */
-/*   Updated: 2019/06/14 11:34:49 by kmodise          ###   ########.fr       */
+/*   Updated: 2019/06/14 14:41:48 by kmodise          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,11 +20,11 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	int		len1;
 	int		len2;
 
-	len1 = ft_strlen(s1);
-	len2 = ft_strlen(s2);
+	len1 = ft_strlen((char *)s1);
+	len2 = ft_strlen((char *)s2);
 	i = 0;
 	k = 0;
-	joint = (char *)malloc((len2 + len2 + 1) * sizeof(char));
+	joint = (char *)malloc((len1 + len2 + 1) * sizeof(char));
 	if (joint == NULL)
 		return (NULL);
 	while (i < len1 + len2)
